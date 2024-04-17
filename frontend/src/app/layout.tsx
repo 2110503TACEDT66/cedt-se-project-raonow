6d2288c093fa58aa6e0a4c20c3ac75eadfbb0d5a
martnarabodee <martnarabodee@gmail.com>
2024-04-17 11:37:46 +0700

    test deployment 1

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -21,7 +21,8 @@ export default async function RootLayout({
 }>) {
 
   const session = await getServerSession(authOptions);
-
+  console.log(session);
+  
   return (
     <html lang="en">
       <body className={inter.className}>
