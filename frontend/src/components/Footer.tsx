7c8193ade6f4c72bb5053315ea0a5d2123b08f78
Xenxa <saipeah1212@gmail.com>
2024-04-18 02:06:09 +0700

    Edit footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,8 +4,8 @@ export default async function Footer () {
     return (
         <div className="h-24 bg-gray-700 bottom-0 left-0 right-0
         flex flex-col space-y-1 justify-center text-white text-center">
-            <a>Hotel Booking Website by Laew Tae Loei</a>
-            <a>This is a part of 2110323 Software Development Practice 2 Project</a>
+            <a>Hotel Booking Website by Rao Now</a>
+            <a>This is a part of 2110423 Software Engineering Project</a>
         </div>
     )
 }
