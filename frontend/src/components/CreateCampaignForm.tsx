913608b59e175f5337c2bd6dcae7247335269dd1
Xenxa <saipeah1212@gmail.com>
2024-04-22 00:30:31 +0700

    Finish design on create campaign

--- frontend/src/components/CreateCampaignForm.tsx
+++ frontend/src/components/CreateCampaignForm.tsx
@@ -73,42 +73,49 @@ export default function CreateCampaignForm({session}:{session:any}) {
     );
 
     return (
-        <main className='bg-gray-100 w-screen h-full flex items-center justify-center flex-col'>
+        <main className='bg-gray-100 w-screen h-[100%] flex flex-col items-center justify-center'>
                 <div className='text-2xl font-bold mt-[20px] flex'>Create Campaign</div>
-                <form className='w-[80vw] mx-auto my-4 p-10 bg-white rounded-lg shadow-lg h-[100%] items-center'>
+                <form className='w-[60vw] mx-auto my-4 p-6 bg-white rounded-lg shadow-lg h-[100%]'>
                     <div className='justify-between'>
-                        <div className="text-left">
-                            <div className="flex justify-center">
-                                <div className="font-semibold flex-col align-center">Title</div>
-                                <div className="flex-col"> <TextField id='title' name='title' className='border px-5 py-2 rounded-md' size="small"
-                                onChange={(e)=>{setTitle(e.target.value)}}></TextField></div>
-                            </div>
-                            <div className="flex justify-center py-1">
-                                <div className="font-semibold flex-col" style={{ verticalAlign: 'top'  }}>Description</div>
-                                <div className="flex flex-col w-[500px]"> <TextField id='description' name='description' className='border px-5 py-2 rounded-md' multiline={true} rows={4}
-                                fullWidth onChange={(e)=>{setDesc(e.target.value)}}></TextField></div>
-                            </div>
-                            <div className="flex justify-center py-1">
-                                <div className="font-semibold flex-col">Point</div>
-                                <div className="flex flex-col w-[446px] "> <TextField id='point' name='point' type="number" className='border px-5 py-2 rounded-md' size="small" fullWidth
-                                value={point < 0 ? 0 : point} onChange={handlePointChange}> </TextField></div>
-                                </div>
-                            {/* <div className='flex space-x-4 w-full items-center'>
-                                <div className="font-semibold">Limited Area</div>
-                                <div><CheckBox/></div>
-                                <Select className='border rounded-md w-1/3'>
-                                    <MenuItem id='Bangkok'>Chiang Mai</MenuItem>
-                                    <MenuItem id='Chiang Mai'>Bangkok</MenuItem>
-                                    <MenuItem id='Chonburi'>Chonburi</MenuItem>
-                                    <MenuItem id='Phuket'>Phuket</MenuItem>
-                                </Select>
-                            </div> */}
-                            {/* <div className='flex space-x-4 w-full items-center'>
-                                <div className="font-semibold">Limit Amount</div><div><CheckBox/></div>
-                                <div> <TextField id='limitAmount' type="number" name='limitAmount' className='border px-5 py-2 rounded-md' size="small"
-                                value={isLimitAmount < 0 ? 0 : isLimitAmount}onChange={handleAmountChange}></TextField></div>
-                            </div> */}
-                        </div>
+                        <table className="text-left border-solid mx-auto mb-3">
+                            <tbody>
+                                <tr className="flex justify-center border-solid">
+                                    <td className="font-semibold flex-col my-2 mr-auto border-solid">Title</td>
+                                    <td className="flex flex-col w-[75%] border-solid"> <TextField id='title' name='title' className='border px-5 py-2 rounded-md' size="small"
+                                    onChange={(e)=>{setTitle(e.target.value)}}></TextField></td>
+                                </tr>
+                                <tr className="flex justify-center border-solid">
+                                    <td className="font-semibold flex-col my-2 mr-auto border-solid" style={{ verticalAlign: 'top'  }}>Description</td>
+                                    <td className="flex flex-col w-[75%] border-solid"> <TextField id='description' name='description' className='border px-5 py-2 rounded-md' multiline={true} rows={4}
+                                    fullWidth onChange={(e)=>{setDesc(e.target.value)}}></TextField></td>
+                                </tr>
+                                <tr className="flex justify-center border-solid">
+                                    <td className="font-semibold flex-col my-2 mr-auto border-solid">Point</td>
+                                    <td className="flex flex-col w-[75%] border-solid"> <TextField id='point' name='point' type="number" className='border px-5 py-2 rounded-md' size="small" fullWidth
+                                    value={point < 0 ? 0 : point} onChange={handlePointChange}> </TextField></td>
+                                </tr>
+                                <tr className='flex border-solid'>
+                                    <td className="font-semibold w-[28%] my-2 border-solid">Limited Area</td>
+                                    <td className="border-solid w-1/6"><Checkbox/></td>
+                                    <td className="border-solid w-2/4">
+                                        <Select className='rounded-md w-[92%] mx-4 my-1' size="small">
+                                            <MenuItem id='Bangkok'>Chiang Mai</MenuItem>
+                                            <MenuItem id='Chiang Mai'>Bangkok</MenuItem>
+                                            <MenuItem id='Chonburi'>Chonburi</MenuItem>
+                                            <MenuItem id='Phuket'>Phuket</MenuItem>
+                                        </Select>
+                                    </td>
+                                </tr>
+                                <tr className='flex border-solid\'>
+                                    <td className="font-semibold w-[28%] my-2 border-solid">Limit Amount</td>
+                                    <td className="border-solid w-1/6"><Checkbox/></td>
+                                    <td className="border-solid w-2/4">
+                                    <TextField id='limitAmount' type="number" name='limitAmount' className='mx-4 my-1 w-[92%] rounded-md' size="small"
+                                    value={isLimitAmount < 0 ? 0 : isLimitAmount}onChange={handleAmountChange}></TextField>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                         <div className="flex w-full items-center justify-center"><button type='submit' onClick={(event)=> {
                             event.preventDefault(); handleCreateCampaign();}}
                             className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-16 
