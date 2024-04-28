c817bc468d43a2c33465890d2c2710e2c3d0c788
Atipat Siwhatrakool <atipatbeam@gmail.com>
2024-04-28 10:23:25 +0700

    add loading

--- frontend/src/components/ReviewPanelBasic.tsx
+++ frontend/src/components/ReviewPanelBasic.tsx
@@ -3,16 +3,17 @@ import { ReviewBasicJSON } from "../../interface";
 export default async function ReviewPanelBasic({reviewBasicInput}: {reviewBasicInput: ReviewBasicJSON}) {
 	const reviewInputJSON = reviewBasicInput;
 	console.log(JSON.stringify(reviewInputJSON));
+	if (!reviewInputJSON) return <div>Loading...</div>;
 	const reviewBasic = reviewInputJSON.data;
-  return (
+  	return (
     <div className="flex flex-col space-y-1 text-lg ">
-			<div>Rating from users</div>
-			<div className="flex flex-row items-end space-x-2 text-blue-600 ">
-				<div className="text-4xl font-medium">{reviewBasic.averageRating.toFixed(1)}</div>
-				<div>/ 5.0</div>
-			</div>
-			<div className="text-xl font-medium">Excellent</div>
-			<div className="text-blue-600">From {reviewBasic.totalReviewCount} reviews</div>
+		<div>Rating from users</div>
+		<div className="flex flex-row items-end space-x-2 text-blue-600 ">
+			<div className="text-4xl font-medium">{reviewBasic.averageRating.toFixed(1)}</div>
+			<div>/ 5.0</div>
+		</div>
+		<div className="text-xl font-medium">Excellent</div>
+		<div className="text-blue-600">From {reviewBasic.totalReviewCount} reviews</div>
 	</div>
   )
 }
