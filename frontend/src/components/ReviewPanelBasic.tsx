c083819566d21d56e72d8982421d8899735e14c4
Atipat Siwhatrakool <atipatbeam@gmail.com>
2024-04-28 10:17:52 +0700

    edit font weight

--- frontend/src/components/ReviewPanelBasic.tsx
+++ frontend/src/components/ReviewPanelBasic.tsx
@@ -8,10 +8,10 @@ export default async function ReviewPanelBasic({reviewBasicInput}: {reviewBasicI
     <div className="flex flex-col space-y-1 text-lg ">
 			<div>Rating from users</div>
 			<div className="flex flex-row items-end space-x-2 text-blue-600 ">
-				<div className="text-4xl">{reviewBasic.averageRating.toFixed(1)}</div>
+				<div className="text-4xl font-medium">{reviewBasic.averageRating.toFixed(1)}</div>
 				<div>/ 5.0</div>
 			</div>
-			<div className="text-xl">Excellent</div>
+			<div className="text-xl font-medium">Excellent</div>
 			<div className="text-blue-600">From {reviewBasic.totalReviewCount} reviews</div>
 	</div>
   )
