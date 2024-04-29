3770a46a64a65420e1f8fc52e8b1458492ab6827
Phakhamon <yok140348@gmail.com>
2024-04-29 16:50:31 +0700

    swagger (review is not finished yet)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,6 +5,147 @@ const router = express.Router();
 
 const {protect, authorize} = require('../middleware/auth');
 
+/**
+* @swagger
+* components:
+*   securitySchemes:
+*     BearerAuth:
+*       type: http
+*       scheme: bearer
+*       bearerFormat: JWT
+*/
+
+//Model
+/**
+* @swagger
+* components:
+*   schemas:
+*     User:
+*       type: object
+*       required:
+*         - name
+*         - telephoneNumber
+*         - email
+*         - password
+*       properties:
+*         id:
+*           type: string
+*           format: uuid
+*         name:
+*           type: string
+*         telephoneNumber:
+*           type: string
+*         email:
+*           type: string
+*         password:
+*           type: string
+*         role:
+*           type: string
+*         member:
+*           type: string
+*           format: uuid
+*         createdAt:
+*           type: string
+*           format: date
+*       example:
+*         id: UID
+*         name: Name
+*         telephoneNumber: "0987654321"
+*         email: any@gmail.com
+*         password: "123456"
+*         role: "admin"
+*         member: null
+*         createAt: 2024-04-18T14:42:41.562Z
+*/
+
+//Tag
+/**
+* @swagger
+* tags:
+*   name: User
+*/
+
+//Register
+/**
+* @swagger
+* /auth/register:
+*   post:
+*     summary: Register
+*     tags: [User]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/User'
+*     responses:
+*       201:
+*         description: Create user successfully
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/User'
+*/
+
+//Login
+/**
+* @swagger
+* /auth/login:
+*   post:
+*     summary: Login
+*     tags: [User]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             required:
+*               - email
+*               - password
+*             properties:
+*               email:
+*                 type: string
+*               password:
+*                 type: string
+*             example:
+*               email: AdminFR@gmail.com
+*               password: "123456"
+*     responses:
+*       200:
+*         description: Successful login
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 token:
+*                   type: string
+*                   example: "<JWT_TOKEN>"
+*       400:
+*         description: Bad request
+*       401:
+*         description: Invalid email or password
+*/
+
+//Get Profile
+/**
+* @swagger
+* /auth/me:
+*   get:
+*     summary: Get your Profile
+*     tags: [User]
+*     security:
+*       - BearerAuth: []
+*     responses:
+*       200:
+*         description: Success
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/User'
+*/
+
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
--- backend/routes/campaign.js
+++ backend/routes/campaign.js
@@ -7,6 +7,183 @@ const router = express.Router();
 
 const {protect, authorize} = require('../middleware/auth');
 
+//Model
+/**
+* @swagger
+* components:
+*   schemas:
+*     Campaign:
+*       type: object
+*       required:
+*         - title
+*         - description
+*         - point
+*         - onePerUser
+*         - discountType
+*         - discountAmount
+*         - totalAmount
+*         - amountLeft
+*         - duration
+*       properties:
+*         id:
+*           type: string
+*           format: uuid
+*         title:
+*           type: string
+*         description:
+*           type: string
+*         point:
+*           type: integer
+*         onePerUser:
+*           type: boolean
+*         discountType:
+*           type: string
+*         discountAmount:
+*           type: integer
+*         limitedArea:
+*           type: string
+*         totalAmount:
+*           type: integer
+*         amountLeft:
+*           type: integer
+*         createdAt:
+*           type: string
+*           format: date
+*         duration:
+*           type: integer
+*/
+
+//Tag
+/**
+* @swagger
+* tags:
+*   name: Campaign
+*/
+
+//Get All
+/**
+* @swagger
+* /campaign:
+*   get:
+*     summary: Returns the list of all campaigns
+*     tags: [Campaign]
+*     security:
+*       - BearerAuth: []
+*     responses:
+*       200:
+*         description: The list of all campaigns
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/Campaign'
+*/
+
+//Get One
+/**
+* @swagger
+* /campaign/{id}:
+*   get:
+*     summary: Returns a campaign
+*     tags: [Campaign]
+*     security:
+*       - BearerAuth: []
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*         required: true
+*     responses:
+*       200:
+*         description: A campaign
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/Campaign'
+*/
+
+//Create
+/**
+* @swagger
+* /campaign:
+*   post:
+*     summary: Create a campaign
+*     tags: [Campaign]
+*     security:
+*       - BearerAuth: []
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/Campaign'
+*     responses:
+*       200:
+*         description: Created campaign successfully
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/Campaign'
+*/
+
+//Update
+/**
+* @swagger
+* /campaign/{id}:
+*   put:
+*     summary: Update a campaign
+*     tags: [Campaign]
+*     security:
+*       - BearerAuth: []
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*         required: true
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/Campaign'
+*     responses:
+*       200:
+*         description: Updated campaign successfully
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/Campaign'
+*/
+
+//Delete
+/**
+* @swagger
+* /campaign/{id}:
+*   delete:
+*     summary: Delete a campaign
+*     tags: [Campaign]
+*     security:
+*       - BearerAuth: []
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*         required: true
+*     responses:
+*       200:
+*         description: The Campaign was deleted
+*/
+
 router.route('/')
     .get(protect, getCampaigns)
     .post(protect, authorize('admin'), createCampaign);
