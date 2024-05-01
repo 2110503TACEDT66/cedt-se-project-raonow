d8a2e2b444542fbc1ce62eb4e215cab1395f5758
Xenxa <saipeah1212@gmail.com>
2024-05-01 08:25:15 +0700

    Add login test

--- frontend/cypress/e2e/epic1.cy.ts
+++ frontend/cypress/e2e/epic1.cy.ts
@@ -17,4 +17,13 @@ describe('member system', () => {
     //
     cy.contains('User created successfully').should('be.visible')
   })
+  it('should login and make booking', ()=>{
+    cy.visit('http://localhost:3000/api/auth/signin')
+    const email = 'cypress@user.com'
+    const password = '111111111'
+    //Login
+    cy.get('input[type="text"]').type(email)
+    cy.get('input[type="password"]').type(password)
+    cy.get('button').click()
+  })
 })
